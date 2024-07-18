af96b4bd03c4a3aa437ca69ea657df8483ad92eb
Amin Nateghi <aminnateghi10@gmail.com>
2024-07-19 03:14:49 +0330

    add token for us bot

--- server.js
+++ server.js
@@ -9,30 +9,30 @@ const TelegramBot = require('node-telegram-bot-api');
 const app = express();
 const port = 3000;
 
-const token = '7190281464:AAFO4AVUtDpHJQ8nCm57zHx_kQX43WtPlQY';
-const chatIds = ['6083550027'];
+const token = '6507821468:AAGdfeHWn3ZrnFtX1W4PDCxdHZ475moujuE';
+const chatIds = ['6083550027','6616187800','5205253618'];
 
 const servers = [
     {url: "https://goldenv.bbbbbsdf.cfd/sd-jklad-mcs-sasdew/xui/", name: "ایران 1"},
-    {url: "https://cruisevpn.bbbbbsdf.cfd/sd-jkgsfdsd-asdfs-sasfhaw/xui/", name: "ایران 2"},
     {url: "https://goldenvppntel.bbbbbsdf.cfd/xui/", name: "هلند 1"},
-    {url: "https://goldenv.bbbbbsdf.cfd/sd-jklad-mcs-sasdew/xui/", name: "آمریکا 1"},
+    {url: "https://cruisevpn.bbbbbsdf.cfd/sd-jkgsfdsd-asdfs-sasfhaw/xui/", name: "ایران 2"},
+    {url: "https://us.bbbbbsdf.cfd/asdfasfwe/xui", name: "آمریکا 2"},
 ]
 
 const bot = new TelegramBot(token, {polling: true});
 
 const downloadDbFile = async () => {
     try {
-        const axios = require('axios');
-
         const config = {
             headers: {
-                'Cookie': 'lang=fa-IR; session=MTcyMDYwNzI5MHxEWDhFQVFMX2dBQUJFQUVRQUFCbF80QUFBUVp6ZEhKcGJtY01EQUFLVEU5SFNVNWZWVk5GVWhoNExYVnBMMlJoZEdGaVlYTmxMMjF2WkdWc0xsVnpaWExfZ1FNQkFRUlZjMlZ5QWYtQ0FBRURBUUpKWkFFRUFBRUlWWE5sY201aGJXVUJEQUFCQ0ZCaGMzTjNiM0prQVF3QUFBQS1fNEk3QVFJQkZXcHJjMkZzYXp0cVptRmxkMnNqSkNWZUppb3FLQUVmSmlvbVhpVW1Ya3BJUlVaSFpYZDVaMlp2ZFdsbVlXeHFhMmh6YUdSa1pnQT18fySHB_payOGsnXC98nImoA_cY8oeUR34IQBdzt7mCbk='
+                'Accept': 'application/octet-stream',
+                'Cookie': 'lang=fa-IR; x-ui=MTcyMDg4NTIwNHxEWDhFQVFMX2dBQUJFQUVRQUFCbF80QUFBUVp6ZEhKcGJtY01EQUFLVEU5SFNVNWZWVk5GVWhoNExYVnBMMlJoZEdGaVlYTmxMMjF2WkdWc0xsVnpaWExfZ1FNQkFRUlZjMlZ5QWYtQ0FBRURBUUpKWkFFRUFBRUlWWE5sY201aGJXVUJEQUFCQ0ZCaGMzTjNiM0prQVF3QUFBQXdfNEl0QVFJQkVtRnZhWE5tSXlRbFhpWmhiM1YzYVdWbWFRRVVZV2x2YUhWbGNtWmhiMmxsSkNWZUppb3FLQ01BfJoWHdCC-KF6fIfdiYrAqsAdpnYg-q2BVtK_sZiafDcN',
             },
             responseType: 'arraybuffer'
         };
 
-        const response = await axios.get('https://goldenv.bbbbbsdf.cfd/sd-jklad-mcs-sasdew/server/getDb', config);
+        const response = await axios.get('https://cruisevpn.bbbbbsdf.cfd/sd-jkgsfdsd-asdfs-sasfhaw/server/getDb', config);
+
         // بررسی اینکه پاسخ دریافتی یک فایل باینری است نه HTML
         if (response.headers['content-type'] !== 'application/octet-stream') {
             console.error('Expected a binary file but received:', response.data.toString('utf8'));
@@ -42,19 +42,15 @@ const downloadDbFile = async () => {
         const filePath = path.join(__dirname, 'database.db');
         fs.writeFileSync(filePath, response.data);
 
-        chatIds.forEach(chatId => {
-            bot.sendDocument(chatId, filePath).catch(error => {
-                console.error(`Failed to send file to chat ${chatId}:`, error);
-            });
+        bot.sendDocument(6616187800, filePath).catch(error => {
+            console.error(`Failed to send file to chat ${6616187800}:`, error);
         });
     } catch (error) {
         console.error('Failed to download or send the database file:', error);
     }
 };
 
-cron.schedule('*/30 * * * *', downloadDbFile);
-
-
+cron.schedule('*/1 * * * *', downloadDbFile);
 
 
 app.use(express.json());
@@ -75,8 +71,9 @@ const pingSite = async (urlToPing, name) => {
     }
 };
 
+
 servers.forEach(item => {
-    cron.schedule('*/3 * * * *', () => {
+    cron.schedule('*/1 * * * *', () => {
         pingSite(item.url, item.name);
     });
 });
@@ -84,6 +81,3 @@ servers.forEach(item => {
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
-
-
-
